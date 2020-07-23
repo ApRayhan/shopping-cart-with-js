581bb13909b46f706c3ee19cbb81e0539d5dd93c
Rayhan <Rayhandimla@gmail.com>
2020-07-23 15:28:10 +0600

    Commeted

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Working ....');
+const plusBtn = document.getElementById('iphonePlus');
+plusBtn.addEventListener('click', function() {
+	// Catch The input Value And Change it
+	const correntTotal = document.getElementById('totalIphone').value;
+	const correntTotalInNumber = parseFloat(correntTotal);
+
+	document.getElementById('totalIphone').value = correntTotalInNumber + 1;
+
+	// Sum The Total Amount
+	const correntAmount = document.getElementById('iphoneAmount').innerText;
+	const correntAmountInNumber = parseFloat(correntAmount);
+
+	document.getElementById('iphoneAmount').innerText = correntAmountInNumber + 1219;
+});
